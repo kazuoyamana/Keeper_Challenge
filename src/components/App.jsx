e3a18d42eb86cb2784550273a4362d84188353a7
Kazuo Yamana <k.yamana@gmail.com>
2020-12-11 13:33:23 +0900

    first commit とりあえず出来たところまで

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([
+    { title: "Test Title", content: "Text Content" },
+    { title: "Test Title2", content: "Text Content2" }
+  ]);
+
   return (
     <div>
       <Header />
       <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      {notes.map((note, index) => {
+        return <Note key={index} title={note.title} content={note.content} />;
+      })}
       <Footer />
     </div>
   );
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 function CreateArea() {
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setNote((prevNote) => {
+      return { ...prevNote, [name]: value };
+    });
+  };
+
+  console.log(note);
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
+        <input
+          onChange={handleChange}
+          name="title"
+          placeholder="Title"
+          value={note.title}
+        />
+        <textarea
+          onChange={handleChange}
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+          value={note.content}
+        />
         <button>Add</button>
       </form>
     </div>
