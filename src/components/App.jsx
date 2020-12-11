b4c75deec962299d740fd9d6c9ff4eca12c4779a
Kazuo Yamana <k.yamana@gmail.com>
2020-12-11 22:51:19 +0900

    add Delete function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,13 +12,30 @@ function App() {
       return [...prevNotes, note];
     });
   };
+
+  const deleteNote = (id) => {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((note, index) => {
+        return index !== id;
+      });
+    });
+  };
+
   return (
     <div>
       <Header />
       <CreateArea onAdd={addNote} />
 
       {notes.map((note, index) => {
-        return <Note key={index} title={note.title} content={note.content} />;
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={note.title}
+            content={note.content}
+            onDelete={deleteNote}
+          />
+        );
       })}
 
       <Footer />
